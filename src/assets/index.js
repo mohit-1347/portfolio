8e1207947792ec1918f6d1327d2096ae4352fa24
Mohit Lalwani <120257953+mohit-1347@users.noreply.github.com>
2025-05-10 08:25:14 +0530

    Update index.js

--- src/assets/index.js
+++ src/assets/index.js
@@ -3,7 +3,7 @@ import bannerImg from "./images/bannerImg.png";
 import projectOne from "./images/projects/project1.jpg";
 import projectTwo from "./images/projects/project2.jpg";
 import projectThree from "./images/projects/project3.jpg";
-import projectFour from "./images/projects/project4.jpg";
+import projectFour from "./images/projects/projectFour.jpg";
 import testimonialOne from "./images/testmonial/testimonialOne.png";
 import testimonialTwo from "./images/testmonial/testimonialTwo.png";
 import quote from "./images/testmonial/quote.png";
